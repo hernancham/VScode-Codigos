39f44deb041deb2438e1f89cf09b659874ad80c0
Hernan <hachambillac@unjbg.edu.pe>
2023-01-31 09:55:09 -0500

    commint inicial

---
+++ Codigos en C++/prueba_c++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string nombre;
+    cout << "escriba su nombre: ";
+    cin >> nombre;
+    cout << "Hola mundo" << endl;
+    cout << "Buen dia " << nombre << endl;
+    return 0;
+}
